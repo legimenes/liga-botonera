14e5c286624c6116609896fd8f54fd4bbd894b53
Leandro G <leandru@gmail.com>
2026-07-21 10:43:05 -0300

    feat: adding handling between lookup and associated inputs

--- src/LigaBotonera/wwwroot/js/lookup2.js
+++ src/LigaBotonera/wwwroot/js/lookup2.js
@@ -14,6 +14,7 @@
             onInput() {
                 if (this.lookupText !== this.lastSearch) {
                     document.getElementById('lookupId-' + this.idSuffix).value = '';
+                    this.dispatchClearEvent();
                 }
                 if (this.lookupText.trim().length === 0) {
                     this.showGrid = false;
@@ -26,6 +27,7 @@
                 setTimeout(() => {
                     if (!document.getElementById('lookupId-' + this.idSuffix).value) {
                         this.lookupText = '';
+                        this.dispatchClearEvent();
                     }
                     document.getElementById('grid-container-' + this.idSuffix).innerHTML = '';
                     this.showGrid = false;
@@ -38,7 +40,8 @@
                 if (singleRow) {
                     const id = singleRow.getAttribute('data-id');
                     const name = singleRow.getAttribute('data-name');
-                    this.onSelectedItem(id, name);
+                    const payload = singleRow.getAttribute('data-payload');
+                    this.onSelectedItem(id, name, payload);
                 }
             },
 
@@ -48,18 +51,30 @@
                 }
             },
 
-            onSelectedItem(id, name) {
+            onSelectedItem(id, name, payload) {
                 this.lookupText = name;
                 this.lastSearch = name;
                 document.getElementById('lookupId-' + this.idSuffix).value = id;
+
+                let dataObj = {};
+                if (payload) {
+                    try { dataObj = JSON.parse(payload); } catch (e) { console.error("Payload parse error", e); }
+                }
+
+                window.dispatchEvent(new CustomEvent('lookup-selected', {
+                    detail: { lookupId: this.idSuffix, id: id, name: name, data: dataObj },
+                    bubbles: true
+                }));
+
                 document.getElementById('grid-container-' + this.idSuffix).innerHTML = '';
                 this.showGrid = false;
+            },
 
-                // Dispara o evento customizado
-                // this.$el.dispatchEvent(new CustomEvent('lookup-selected', {
-                //     detail: { id: id, name: nome },
-                //     bubbles: true
-                // }));
+            dispatchClearEvent() {
+                this.$el.dispatchEvent(new CustomEvent('lookup-cleared', {
+                    detail: { lookupId: this.idSuffix },
+                    bubbles: true
+                }));
             }
         }));
     };
